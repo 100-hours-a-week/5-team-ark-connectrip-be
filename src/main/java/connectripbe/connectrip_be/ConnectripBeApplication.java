77ae037ea5f0eed8e78512fd514faa218d925ab4
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2024-08-13 15:37:21 +0900

    add: 프로젝트 설정 파일 및 템플릿 추가
    - application.yml, application-local.yml, application-prod.yml 파일 추가
    - 버그 리포트 및 기능 요청 템플릿 추가
    - Gradle 설정 파일 추가
    - 초기 Spring Boot 프로젝트 파일 및 테스트 추가

---
+++ src/main/java/connectripbe/connectrip_be/ConnectripBeApplication.java
@@ -0,0 +1,13 @@
+package connectripbe.connectrip_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectripBeApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run(ConnectripBeApplication.class, args);
+      }
+
+}
---
+++ src/test/java/connectripbe/connectrip_be/ConnectripBeApplicationTests.java
@@ -0,0 +1,13 @@
+package connectripbe.connectrip_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectripBeApplicationTests {
+
+      @Test
+      void contextLoads() {
+      }
+
+}
