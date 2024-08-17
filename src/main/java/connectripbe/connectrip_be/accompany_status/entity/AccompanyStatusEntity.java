5f6674c8b50a6321706365e5bda67a88a2ff6d74
49EHyeon42 <49ehyeon42@gmail.com>
2024-08-17 11:51:36 +0900

    feat: 동행 상태 엔티티, 열거형, 레포 추가

---
+++ src/main/java/connectripbe/connectrip_be/accompany_status/entity/AccompanyStatusEntity.java
@@ -0,0 +1,30 @@
+package connectripbe.connectrip_be.accompany_status.entity;
+
+import connectripbe.connectrip_be.post.entity.AccompanyPostEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class AccompanyStatusEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "accompany_post_id", nullable = false)
+    private AccompanyPostEntity accompanyPostEntity;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private AccompanyStatusEnum accompanyStatusEnum;
+
+    public AccompanyStatusEntity(AccompanyPostEntity accompanyPostEntity, AccompanyStatusEnum accompanyStatusEnum) {
+        this.accompanyPostEntity = accompanyPostEntity;
+        this.accompanyStatusEnum = accompanyStatusEnum;
+    }
+}
---
+++ src/main/java/connectripbe/connectrip_be/accompany_status/repository/AccompanyStatusJpaRepository.java
@@ -0,0 +1,8 @@
+package connectripbe.connectrip_be.accompany_status.repository;
+
+import connectripbe.connectrip_be.accompany_status.entity.AccompanyStatusEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccompanyStatusJpaRepository extends JpaRepository<AccompanyStatusEntity, Long> {
+
+}
