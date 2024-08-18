6aa069d711a700b1fb89c8f45bf6cccf8f082929
true <truee030@gmail.com>
2024-08-18 20:19:08 +0900

    feat: AccompanyCommentServiceImpl에 각 메서들마다 주석 작성

--- src/main/java/connectripbe/connectrip_be/comment/service/impl/AccompanyCommentServiceImpl.java
+++ src/main/java/connectripbe/connectrip_be/comment/service/impl/AccompanyCommentServiceImpl.java
@@ -25,6 +25,15 @@ public class AccompanyCommentServiceImpl implements AccompanyCommentService {
     private final MemberJpaRepository memberRepository;
     private final AccompanyPostRepository accompanyPostRepository;
 
+    /**
+     * 댓글을 생성하는 메서드.
+     * 사용자 이메일을 통해 MemberEntity를 조회하고, 게시물 ID를 통해 AccompanyPostEntity를 조회한 후
+     * AccompanyCommentEntity를 생성하여 데이터베이스에 저장
+     *
+     * @param request 댓글 생성 요청 정보 (게시물 ID, 댓글 내용 포함)
+     * @param email 댓글 작성자의 이메일
+     * @return 생성된 댓글의 정보를 담은 AccompanyCommentResponse 객체
+     */
     @Override
     @Transactional
     public AccompanyCommentResponse createComment(AccompanyCommentRequest request, String email) {
@@ -35,6 +44,12 @@ public class AccompanyCommentServiceImpl implements AccompanyCommentService {
         return AccompanyCommentResponse.fromEntity(accompanyCommentRepository.save(comment));
     }
 
+    /**
+     * 댓글을 삭제하는 메서드.
+     * 주어진 댓글 ID를 통해 AccompanyCommentEntity를 조회한 후, 해당 댓글을 데이터베이스에서 삭제
+     *
+     * @param commentId 삭제할 댓글의 ID
+     */
     @Override
     @Transactional
     public void deleteComment(Long commentId) {
@@ -42,6 +57,13 @@ public class AccompanyCommentServiceImpl implements AccompanyCommentService {
         accompanyCommentRepository.delete(comment);
     }
 
+    /**
+     * 특정 게시물에 달린 모든 댓글을 조회하는 메서드.
+     * 주어진 게시물 ID를 통해 해당 게시물에 달린 댓글 목록을 조회한 후, 이를 DTO로 변환하여 반환
+     *
+     * @param postId 댓글을 조회할 게시물의 ID
+     * @return 해당 게시물에 달린 댓글들의 정보를 담은 List<AccompanyCommentResponse> 객체
+     */
     @Override
     @Transactional(readOnly = true)
     public List<AccompanyCommentResponse> getCommentsByPost(Long postId) {
@@ -51,16 +73,37 @@ public class AccompanyCommentServiceImpl implements AccompanyCommentService {
                 .toList();
     }
 
+    /**
+     * 주어진 댓글 ID로 댓글을 조회하는 메서드.
+     * 만약 해당 댓글이 존재하지 않으면 GlobalException을 발생
+     *
+     * @param commentId 조회할 댓글의 ID
+     * @return 조회된 AccompanyCommentEntity 객체
+     */
     private AccompanyCommentEntity getComment(Long commentId) {
         return accompanyCommentRepository.findById(commentId)
                 .orElseThrow(() -> new GlobalException(ErrorCode.COMMENT_NOT_FOUND));
     }
 
+    /**
+     * 주어진 게시물 ID로 게시물을 조회하는 메서드.
+     * 만약 해당 게시물이 존재하지 않으면 GlobalException을 발생
+     *
+     * @param postId 조회할 게시물의 ID
+     * @return 조회된 AccompanyPostEntity 객체
+     */
     private AccompanyPostEntity getPost(Long postId) {
         return accompanyPostRepository.findById(postId)
                 .orElseThrow(() -> new GlobalException(ErrorCode.NOT_FOUND_ACCOMPANY_POST));
     }
 
+    /**
+     * 주어진 이메일로 회원을 조회하는 메서드.
+     * 만약 해당 이메일로 등록된 회원이 존재하지 않으면 GlobalException을 발생
+     *
+     * @param email 조회할 회원의 이메일
+     * @return 조회된 MemberEntity 객체
+     */
     private MemberEntity getMember(String email) {
         return memberRepository.findByEmail(email)
                 .orElseThrow(() -> new GlobalException(ErrorCode.NOT_FOUND_MEMBER));
