e938f87cb278fb83af15f1df4ee28f8de8ed38d4
true <truee030@gmail.com>
2024-08-19 17:54:19 +0900

    feat: 댓글 날짜 변수명 수정 & response Body 수정

--- src/main/java/connectripbe/connectrip_be/comment/dto/AccompanyCommentResponse.java
+++ src/main/java/connectripbe/connectrip_be/comment/dto/AccompanyCommentResponse.java
@@ -20,9 +20,9 @@ public class AccompanyCommentResponse {
     private Long memberId;  // 사용자 아이디
     private Long accompanyPostId;  // 동행 아이디
     private String content;  // 내용
-    private LocalDateTime createdDate;  // 생성 일자
-    private LocalDateTime deletedDate;  // 삭제 일자 (null 가능)
-    private LocalDateTime updatedAt; // 수정 일자
+    private LocalDateTime createdAt;  // 생성 일자
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;  // 삭제 일자 (null 가능)
 
     // 엔티티를 DTO로 변환하는 메서드
     public static AccompanyCommentResponse fromEntity(AccompanyCommentEntity comment) {
@@ -31,6 +31,9 @@ public class AccompanyCommentResponse {
                 .memberId(comment.getMemberEntity().getId())
                 .accompanyPostId(comment.getAccompanyPostEntity().getId())
                 .content(comment.getContent())
+                .createdAt(comment.getCreatedAt())
+                .updatedAt(comment.getUpdatedAt())
+                .deletedAt(comment.getDeletedAt())
                 .build();
     }
 }
