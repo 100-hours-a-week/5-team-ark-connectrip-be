57f524b7c6df5098ae6b9b421650cbfb259c10cb
49EHyeon42 <49ehyeon42@gmail.com>
2024-08-21 10:21:15 +0900

    feat: 이메일 중복 확인 API 추가

--- src/main/java/connectripbe/connectrip_be/member/service/MemberServiceImpl.java
+++ src/main/java/connectripbe/connectrip_be/member/service/MemberServiceImpl.java
@@ -1,6 +1,7 @@
 package connectripbe.connectrip_be.member.service;
 
 import connectripbe.connectrip_be.global.dto.GlobalResponse;
+import connectripbe.connectrip_be.member.dto.CheckDuplicateEmailDto;
 import connectripbe.connectrip_be.member.dto.FirstUpdateMemberRequest;
 import connectripbe.connectrip_be.member.dto.MemberHeaderInfoDto;
 import connectripbe.connectrip_be.member.entity.MemberEntity;
@@ -12,16 +13,24 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
     private final MemberJpaRepository memberJpaRepository;
+
+    @Transactional(readOnly = true)
+    @Override
+    public GlobalResponse<CheckDuplicateEmailDto> checkDuplicateEmail(String email) {
+        return new GlobalResponse<>("SUCCESS", new CheckDuplicateEmailDto(memberJpaRepository.existsByEmail(email)));
+    }
 
     /**
      * 헤더에 사용자 프로필 이미지와 닉네임을 전달하기 위한 메서드
      *
      * @author noah(49EHyeon42)
      */
+    @Transactional(readOnly = true)
     @Override
     public GlobalResponse<MemberHeaderInfoDto> getMemberHeaderInfo(String email) {
         MemberEntity memberEntity = memberJpaRepository.findByEmail(email)
