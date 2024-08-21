fbff7c0b5af19811e224d0b9e8c3ecdefb486d75
true <truee030@gmail.com>
2024-08-21 14:55:18 +0900

    fix: 빌드 패턴 수정

--- src/main/java/connectripbe/connectrip_be/Review/dto/AccompanyReviewRequest.java
+++ src/main/java/connectripbe/connectrip_be/Review/dto/AccompanyReviewRequest.java
@@ -1,22 +1,17 @@
 package connectripbe.connectrip_be.Review.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor  // 기본 생성자 필요
+@AllArgsConstructor  // 모든 필드를 포함하는 생성자를 Lombok이 자동으로 생성
+@Builder  // 빌더 패턴을 사용하기 위함
 public class AccompanyReviewRequest {
     private Long reviewerId;
     private Long targetId;
     private Long accompanyPostId;
     private String content;
-
-    @Builder
-    public AccompanyReviewRequest(Long reviewerId, Long targetId, Long accompanyPostId, String content) {
-        this.reviewerId = reviewerId;
-        this.targetId = targetId;
-        this.accompanyPostId = accompanyPostId;
-        this.content = content;
-    }
 }
