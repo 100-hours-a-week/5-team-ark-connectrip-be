da92ab112ab4da524636bf7e63f85df2fbe642c8
49EHyeon42 <49ehyeon42@gmail.com>
2024-08-21 09:26:19 +0900

    fix: 헤더 사용자 프로필 이미지, 닉네임에 대한 응답 변경

--- src/main/java/connectripbe/connectrip_be/member/service/MemberService.java
+++ src/main/java/connectripbe/connectrip_be/member/service/MemberService.java
@@ -1,12 +1,13 @@
 package connectripbe.connectrip_be.member.service;
 
+import connectripbe.connectrip_be.global.dto.GlobalResponse;
 import connectripbe.connectrip_be.member.dto.FirstUpdateMemberRequest;
-import connectripbe.connectrip_be.member.dto.MemberHeaderInfoResponse;
+import connectripbe.connectrip_be.member.dto.MemberHeaderInfoDto;
 
 public interface MemberService {
 
-    MemberHeaderInfoResponse getMemberHeaderInfo(String email);
+    GlobalResponse<MemberHeaderInfoDto> getMemberHeaderInfo(String email);
 
     // fixme-noah: 추후 달라지면 response 분리
-    MemberHeaderInfoResponse getFirstUpdateMemberResponse(String email, FirstUpdateMemberRequest request);
+    GlobalResponse<MemberHeaderInfoDto> getFirstUpdateMemberResponse(String email, FirstUpdateMemberRequest request);
 }
