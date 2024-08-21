d35484e709304bde92d18966999301ee57b9aec5
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2024-08-22 01:59:26 +0900

    fix: 마지막 채팅 시간 초기화 문제 수정
    - 마지막 채팅 시간이 없을 경우 생성 시간으로 대체하는 로직 추가.
    - NullPointerException 방지를 위해 lastChatTime 처리 개선.
    - GlobalExceptionHandler에 일반 예외 처리를 위한 핸들러 추가.

--- src/main/java/connectripbe/connectrip_be/global/exception/GlobalExceptionHandler.java
+++ src/main/java/connectripbe/connectrip_be/global/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package connectripbe.connectrip_be.global.exception;
 
 
+import static connectripbe.connectrip_be.global.exception.type.ErrorCode.INTERNAL_SERVER_ERROR;
+
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -15,6 +18,12 @@ public class GlobalExceptionHandler {
 
         return new ErrorResponse(e.getErrorCode(),
             e.getErrorCode().getHttpStatus(), e.getErrorCode().getDescription());
+      }
+      @ExceptionHandler(Exception.class)
+      public ErrorResponse exceptionHandler(Exception e) {
+            log.error("Exception is occurred", e);
+            return new ErrorResponse(INTERNAL_SERVER_ERROR, HttpStatus.INTERNAL_SERVER_ERROR,
+                    INTERNAL_SERVER_ERROR.getDescription());
       }
 
 }
