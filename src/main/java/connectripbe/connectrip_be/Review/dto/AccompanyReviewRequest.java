f87b14f035b483593dbf181612b3e47c644c3101
true <truee030@gmail.com>
2024-08-21 14:50:04 +0900

    fix: 오류 수정

--- src/main/java/connectripbe/connectrip_be/Review/dto/AccompanyReviewRequest.java
+++ src/main/java/connectripbe/connectrip_be/Review/dto/AccompanyReviewRequest.java
@@ -2,12 +2,21 @@ package connectripbe.connectrip_be.Review.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
+@NoArgsConstructor
 public class AccompanyReviewRequest {
     private Long reviewerId;
     private Long targetId;
     private Long accompanyPostId;
     private String content;
+
+    @Builder
+    public AccompanyReviewRequest(Long reviewerId, Long targetId, Long accompanyPostId, String content) {
+        this.reviewerId = reviewerId;
+        this.targetId = targetId;
+        this.accompanyPostId = accompanyPostId;
+        this.content = content;
+    }
 }
