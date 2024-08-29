694f64aa422325992dffb0b5a95016c3ed1459d2
true <truee030@gmail.com>
2024-08-29 17:01:48 +0900

    fix: customUrl 게시글 조회 메서드 추가

--- src/main/java/connectripbe/connectrip_be/post/repository/AccompanyPostRepository.java
+++ src/main/java/connectripbe/connectrip_be/post/repository/AccompanyPostRepository.java
@@ -1,10 +1,8 @@
 package connectripbe.connectrip_be.post.repository;
 
 import connectripbe.connectrip_be.post.entity.AccompanyPostEntity;
-
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +10,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccompanyPostRepository extends JpaRepository<AccompanyPostEntity, Long> {
+
+    Optional<AccompanyPostEntity> findByCustomUrl(String customUrl);
 
     Optional<AccompanyPostEntity> findByIdAndDeletedAtIsNull(long id);
 
