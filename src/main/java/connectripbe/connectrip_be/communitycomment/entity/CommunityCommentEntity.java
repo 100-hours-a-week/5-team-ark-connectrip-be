fa0aaceb1e49b12a411ee077e904adbf1ab374ea
true <truee030@gmail.com>
2024-09-04 15:36:34 +0900

    feat: community comment 작성, 수정, 조회 Entity 구현

--- src/main/java/connectripbe/connectrip_be/communitycomment/entity/CommunityCommentEntity.java
+++ src/main/java/connectripbe/connectrip_be/communitycomment/entity/CommunityCommentEntity.java
@@ -1,4 +1,46 @@
 package connectripbe.connectrip_be.communitycomment.entity;
 
-public class CommunityCommentEntity {
+import connectripbe.connectrip_be.communitypost.entity.CommunityPostEntity;
+import connectripbe.connectrip_be.global.entity.BaseEntity;
+import connectripbe.connectrip_be.member.entity.MemberEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "community_comment")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Builder
+public class CommunityCommentEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private MemberEntity memberEntity;  // 사용자 아이디 (외래키)
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "community_post_id", nullable = false)
+    private CommunityPostEntity communityPostEntity;  // 커뮤니티 아이디 (외래키)
+
+    @Setter
+    @Column(nullable = false, length = 256)
+    private String content;  // 내용
+
 }
