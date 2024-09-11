e1ca82f31740a402a66de879d028645f2a3a0c0d
true <truee030@gmail.com>
2024-09-11 14:50:12 +0900

    fix: 리뷰 Response 수정

--- src/main/java/connectripbe/connectrip_be/Review/dto/AccompanyReviewResponse.java
+++ src/main/java/connectripbe/connectrip_be/Review/dto/AccompanyReviewResponse.java
@@ -1,36 +1,31 @@
 package connectripbe.connectrip_be.Review.dto;
 
 import connectripbe.connectrip_be.Review.entity.AccompanyReviewEntity;
-import lombok.Builder;
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import lombok.Builder;
+import lombok.Getter;
 
 @Getter
 @Builder
 public class AccompanyReviewResponse {
     private Long reviewId;
-    private Long reviewerId;
-    private Long targetId;
-    private Long chatRoomId;
     private String content;
+    private String reviewerNickname;
+    private String reviewerProfile;
+    private Long reviewerId;
     private String createdAt;
-    private String updatedAt;
-    private String deletedAt;
 
     // 엔티티를 DTO로 변환하는 메서드
     public static AccompanyReviewResponse fromEntity(AccompanyReviewEntity review) {
         return AccompanyReviewResponse.builder()
                 .reviewId(review.getId())
-                .reviewerId(review.getReviewer().getId())
-                .targetId(review.getTarget().getId())
-                .chatRoomId(review.getChatRoom().getId())
                 .content(review.getContent())
-                .createdAt(formatToUTC(review.getCreatedAt()))
-                .updatedAt(formatToUTC(review.getUpdatedAt()))
-                .deletedAt(formatToUTC(review.getDeletedAt()))
+                .reviewerNickname(review.getReviewer().getNickname()) // 작성자 닉네임
+                .reviewerProfile(review.getReviewer().getProfileImagePath()) // 작성자 프로필 이미지 경로
+                .reviewerId(review.getReviewer().getId()) // 작성자 ID
+                .createdAt(formatToUTC(review.getCreatedAt())) // 작성일
                 .build();
     }
 
