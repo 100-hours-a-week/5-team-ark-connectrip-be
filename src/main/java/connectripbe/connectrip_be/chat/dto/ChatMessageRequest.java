ef4ff982080bca7132fdd3b978e63ec280a94b3c
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2024-09-20 11:28:51 +0900

    refactor: ChatMessageRequest에서 senderId 타입 변경
    - senderId의 타입을 Long에서 String으로 변경했습니다.
    - 불필요한 커스텀 역직렬화기 어노테이션을 삭제했습니다.
    
    Signed-off-by: Namgyu11 <103015031+Namgyu11@users.noreply.github.com>

--- src/main/java/connectripbe/connectrip_be/chat/dto/ChatMessageRequest.java
+++ src/main/java/connectripbe/connectrip_be/chat/dto/ChatMessageRequest.java
@@ -1,15 +1,13 @@
 package connectripbe.connectrip_be.chat.dto;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import connectripbe.connectrip_be.global.converter.StringToLongDeserializer;
+
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
 public record ChatMessageRequest(
         Long chatRoomId,
-        @JsonDeserialize(using = StringToLongDeserializer.class) // 커스텀 역직렬화기 사용
-        Long senderId,
+        String senderId,
         @Size(max = 256, message = "채팅 내용은 256자 이내로 입력해주세요.")
         String content,
         Boolean infoFlag
