c6a5ae3446395526a48ca3b369e6901fbfdb42e9
true <truee030@gmail.com>
2024-09-22 17:52:24 +0900

    fix: 게시글 status를 최신 상태로 가져오는 쿼리 제거 (기존꺼 사용)

--- src/main/java/connectripbe/connectrip_be/accompany_status/repository/AccompanyStatusJpaRepository.java
+++ src/main/java/connectripbe/connectrip_be/accompany_status/repository/AccompanyStatusJpaRepository.java
@@ -4,15 +4,10 @@ import connectripbe.connectrip_be.accompany_status.entity.AccompanyStatusEntity;
 import connectripbe.connectrip_be.post.entity.AccompanyPostEntity;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface AccompanyStatusJpaRepository extends JpaRepository<AccompanyStatusEntity, Long> {
 
     Optional<AccompanyStatusEntity> findTopByAccompanyPostEntityOrderByCreatedAtDesc(
             AccompanyPostEntity accompanyPostEntity);
 
-    @Query("SELECT a FROM AccompanyStatusEntity a WHERE a.accompanyPostEntity = :accompanyPostEntity ORDER BY a.createdAt DESC")
-    Optional<AccompanyStatusEntity> findLatestStatusByPost(
-            @Param("accompanyPostEntity") AccompanyPostEntity accompanyPostEntity);
 }
