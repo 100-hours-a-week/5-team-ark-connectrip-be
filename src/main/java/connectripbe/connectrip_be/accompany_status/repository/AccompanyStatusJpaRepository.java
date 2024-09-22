b5c748c5a3631917e52b396d654eff61eee73ee4
true <truee030@gmail.com>
2024-09-22 17:48:20 +0900

    fix: 게시글 status를 최신 상태로 가져오는 쿼리 추가

--- src/main/java/connectripbe/connectrip_be/accompany_status/repository/AccompanyStatusJpaRepository.java
+++ src/main/java/connectripbe/connectrip_be/accompany_status/repository/AccompanyStatusJpaRepository.java
@@ -2,11 +2,17 @@ package connectripbe.connectrip_be.accompany_status.repository;
 
 import connectripbe.connectrip_be.accompany_status.entity.AccompanyStatusEntity;
 import connectripbe.connectrip_be.post.entity.AccompanyPostEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AccompanyStatusJpaRepository extends JpaRepository<AccompanyStatusEntity, Long> {
 
-    Optional<AccompanyStatusEntity> findTopByAccompanyPostEntityOrderByCreatedAtDesc(AccompanyPostEntity accompanyPostEntity);
+    Optional<AccompanyStatusEntity> findTopByAccompanyPostEntityOrderByCreatedAtDesc(
+            AccompanyPostEntity accompanyPostEntity);
+
+    @Query("SELECT a FROM AccompanyStatusEntity a WHERE a.accompanyPostEntity = :accompanyPostEntity ORDER BY a.createdAt DESC")
+    Optional<AccompanyStatusEntity> findLatestStatusByPost(
+            @Param("accompanyPostEntity") AccompanyPostEntity accompanyPostEntity);
 }
