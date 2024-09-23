74b988d97024ab95aaeac396a9d71d88f22d3110
49EHyeon42 <49ehyeon42@gmail.com>
2024-09-23 20:31:55 +0900

    feat: 동행 그룹, 내가 작성한 후기 조회 API 구현

---
+++ src/main/java/connectripbe/connectrip_be/review/dto/AccompanyReviewSummaryResponse.java
@@ -0,0 +1,8 @@
+package connectripbe.connectrip_be.review.dto;
+
+public record AccompanyReviewSummaryResponse(
+        String revieweeNickname,
+        String content,
+        String createdAt
+) {
+}
