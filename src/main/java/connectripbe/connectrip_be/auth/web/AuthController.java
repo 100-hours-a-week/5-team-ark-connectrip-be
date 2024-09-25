e1dbc6ba4e7fd2d63ed51357604a408f603e98e9
49EHyeon42 <49ehyeon42@gmail.com>
2024-09-25 17:37:37 +0900

    test: 쿠키 Secure, SameSite 속성 테스트 복구

--- src/main/java/connectripbe/connectrip_be/auth/web/AuthController.java
+++ src/main/java/connectripbe/connectrip_be/auth/web/AuthController.java
@@ -13,7 +13,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -49,23 +48,12 @@ public class AuthController {
 
         try {
             if (tokenDto.isFirstLogin()) {
-                ResponseCookie tempTokenCookie = ResponseCookie.from("tempToken", tokenDto.getTempToken())
-                        .path("/")
-                        .httpOnly(true)
-                        .maxAge(tokenDto.getTempTokenExpirationTime())
-                        .domain("connectrip.site")
-//                        .sameSite("None")
-                        .secure(true)
-                        .build();
+                Cookie tempTokenCookie = new Cookie("tempToken", tokenDto.getTempToken());
+                tempTokenCookie.setPath("/");
+                tempTokenCookie.setMaxAge(tokenDto.getTempTokenExpirationTime());
+                tempTokenCookie.setHttpOnly(true);
 
-                httpServletResponse.addHeader("Set-Cookie", tempTokenCookie.toString());
-
-//                Cookie tempTokenCookie = new Cookie("tempToken", tokenDto.getTempToken());
-//                tempTokenCookie.setPath("/");
-//                tempTokenCookie.setMaxAge(tokenDto.getTempTokenExpirationTime());
-//                tempTokenCookie.setHttpOnly(true);
-//
-//                httpServletResponse.addCookie(tempTokenCookie);
+                httpServletResponse.addCookie(tempTokenCookie);
 
                 httpServletResponse.sendRedirect(kakaoFirstLoginRedirectUrl);
             } else {
